5f0c97060a8383e591b6810aa316dbf11e8bb81d
Bajrang Sharan Singh <86580690+bssingh01@users.noreply.github.com>
2024-03-29 22:27:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+let hours = document.querySelector("#hours");
+let minutes = document.querySelector("#minutes");
+let seconds = document.querySelector("#seconds");
+let ampm = document.querySelector("#ampm");
+
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+
+  // Logic for setting time in 12 hours format
+  h = h > 12 ? h - 12 : h;
+
+  // Logic for adding one zero in starting if h, m, s value is less than 10.
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  //Setting output in respective UI elements
+  hours.innerText = h;
+  minutes.innerText = m;
+  seconds.innerText = s;
+}
+console.log("hello");
+setInterval(updateClock, 1000);
